9628e5bc8df19e5a700bbe294887a6f8e74d6587
kevin.mutua@student.moringaschool.com <kevin.mutua@student.moringaschool.com>
2023-03-07 09:35:42 +0300

    completed

---
+++ lib/insert.sql
@@ -0,0 +1,8 @@
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Mr. Chocolate", 57, "M", "white", "fast", "yes");
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Rowdy", 78, "M", "yellow", "fast", "yes");
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Tabitha", 65,"F", "white", "slow", "no");
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Sergeant Brown", 57, "M", "green", "moderate", "yes");
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Melissa", 44, "F", "blue", "sad", "yes");
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Grinch", 35, "M", "pink", "fast", "no");
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Wendy", 27, "F", "black", "fast", "yes");
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES (NULL, 57, "M", "white", "fast", "yes");
